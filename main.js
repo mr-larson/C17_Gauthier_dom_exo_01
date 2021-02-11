b8e7f94fdae85e18599fc726984c8f19220e6d07
GAUTHIER <gauthierdewit@gmail.com>
2021-02-11 22:20:00 +0100

    dom01

---
+++ main.js
@@ -0,0 +1,14 @@
+let h1 = document.getElementsByTagName("h1")
+console.log (h1)
+
+let h3_0 = document.getElementsByTagName("h3") [0]
+let h3_1 = document.getElementsByTagName("h3") [1]
+let h3_2 = document.getElementsByTagName("h3") [2]
+console.log (h3_0, h3_1, h3_2)
+
+let p = document.getElementsByTagName ("p") [1]
+console.log (p)
+
+let li = document.getElementsByTagName("li")
+let li_2 = document.getElementsByTagName("li") [2]
+console.log (li_2)
